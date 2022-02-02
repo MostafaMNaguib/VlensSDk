2b59af887acb5ed4e30485c86b1c9962c2376b18
mostanaguib <mostafanagieb@codecaique.com>
2022-02-02 12:35:31 +0200

    Initial commit

---
+++ first-sdk/src/main/java/com/bitbang/first_sdk/Step1Fragment.kt
@@ -0,0 +1,28 @@
+package com.bitbang.first_sdk
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class Step1Fragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_step1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+}
